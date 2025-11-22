2ed822a3e756de628d380e202461a2683304793d
Aarif <adebayoabdulazeez195@gmail.com>
2025-11-22 17:08:20 +0100

    feat: implement  likeComment to Postgres with strict PRIVATE behavior

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -705,9 +705,8 @@ async function replyComment(req, res) {
 }
 
 // like/unlike a comment
-function likeComment(req, res) {
-
-    const user = authController.authenticate(req);
+async function likeComment(req, res) {
+    const user = await authController.authenticate(req);
     if (!user) {
         res.writeHead(401, { "Content-Type": "application/json" });
         return res.end(JSON.stringify({ message: "Unauthorized" }));
@@ -717,44 +716,61 @@ function likeComment(req, res) {
     const articleId = parseInt(urlParts[3]);
     const commentId = parseInt(urlParts[5]);
 
-    const data = JSON.parse(fs.readFileSync(file, "utf8"));
-    const article = data.find(a => a.id === articleId);
+    try {
+        // Fetch article
+        const articleResult = await pool.query(
+            `SELECT * FROM articles WHERE id = $1`,
+            [articleId]
+        );
+        const article = articleResult.rows[0];
 
-    if (!article) {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ message: "Article not found" }));
+        if (!article) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            return res.end(JSON.stringify({ message: "Article not found" }));
+        }
+
+        // STRICT PRIVATE: only article owner can like/unlike
+        if (article.author !== user.username) {
+            res.writeHead(403, { "Content-Type": "application/json" });
+            return res.end(JSON.stringify({ message: "Forbidden: Only the article owner can like/unlike comments" }));
+        }
+
+        // Parse comments JSONB
+        const comments = article.comments || [];
+        const commentIndex = comments.findIndex(c => c.id === commentId);
+
+        if (commentIndex === -1) {
+            res.writeHead(404, { "Content-Type": "application/json" });
+            return res.end(JSON.stringify({ message: "Comment not found" }));
+        }
+
+        // Toggle like/unlike
+        if (typeof comments[commentIndex].liked === "undefined") comments[commentIndex].liked = false;
+
+        let message;
+        if (comments[commentIndex].liked) {
+            comments[commentIndex].liked = false;
+            message = "Comment unliked!";
+        } else {
+            comments[commentIndex].liked = true;
+            message = "Comment liked!";
+        }
+
+        // Update article in Postgres
+        await pool.query(
+            `UPDATE articles SET comments = $1 WHERE id = $2`,
+            [JSON.stringify(comments), articleId]
+        );
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message, comment: comments[commentIndex] }));
+    } catch (err) {
+        console.error(err);
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Server error" }));
     }
-
-    const comment = article.comments.find(c => c.id === commentId);
-    if (!comment) {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ message: "Comment not found" }));
-    }
-
-    // Initialize liked if not present
-    if (typeof comment.liked === "undefined") comment.liked = false;
-
-    if (comment.user !== user.username) {
-        res.writeHead(403, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ message: "You are not allowed to like to this comment" }));
-    }
-
-    let message;
-    if (comment.liked) {
-        comment.liked = false;
-        message = "Comment unliked!";
-    } else {
-        comment.liked = true;
-        message = "Comment liked!";
-    }
-
-
-
-    fs.writeFileSync(file, JSON.stringify(data, null, 2));
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message, comment }));
 }
+
 
 // like/unlike a reply
 function likeReply(req, res) {
